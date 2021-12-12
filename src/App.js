645951d97b49676f5f78e81980d03d5a5694ba44
Md Shakil Chowdhury <ms181161@gmail.com>
2021-12-12 16:49:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,20 +7,6 @@ function App() {
   return (
     <div className="App">
       <h1>Todo App By T-4</h1>
-      {/* <motion.h1
-        initial={{ y: -200 }} 
-        animate={{ y: 0 }}
-        transition={{ type: "spring", duration: 0.5 }}
-        whileHover={{ scale: 1.1 }}
-      >
-       
-      </motion.h1> */}
-      {/* <motion.div
-        initial={{ y: 1000 }}
-        animate={{ y: 0 }}
-        transition={{ type: 'spring', duration: 1 }}
-      >
-      </motion.div> */}
       <Todos />
       <DisplayTodos />
     </div>
